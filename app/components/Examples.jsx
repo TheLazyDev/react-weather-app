33fe5f4cb9fc3aaf3dfb20ceaaa547eca38fd7d2
Bhavarsh Singh <bhavarshs@gmail.com>
2017-03-13 17:20:44 +0530

    Added copy

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -7,7 +7,11 @@ var React = require('react');
 var Examples = (props) => {
   
       return (
+
+            <div>
             <h3> Examples Component </h3>
+            <p> Welcome to Examples page </p>
+            </div>
         )
 }
 
